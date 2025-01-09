dfed3c626134a3430ce02942f2512d9a20b3eaf1
Naza Bakhlika <bnaza98@gmail.com>
2025-01-09 13:07:58 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Hello World!!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
